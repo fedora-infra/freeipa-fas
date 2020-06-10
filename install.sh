b82302d96efc16584a064e6ac40f250b0c7b19a1
Christian Heimes <cheimes@redhat.com>
2020-06-10 12:45:50 +0200

    Run our updates before 90-post_upgrade_plugins (#120)

--- install.sh
+++ install.sh
@@ -3,15 +3,17 @@ set -e
 
 SITE_PACKAGES=$(python3 -c 'from sys import version_info as v; print(f"/usr/lib/python{v.major}.{v.minor}/site-packages")')
 
-if [ -f /usr/share/ipa/schema.d/99-fasschema.ldif -a -f /usr/share/ipa/updates/99-fas.update ]; then
+if [ -f /usr/share/ipa/schema.d/89-fasschema.ldif -a -f /usr/share/ipa/updates/89-fas.update ]; then
     NEEDS_UPGRADE=0;
 else
     NEEDS_UPGRADE=1;
 fi
 
-cp schema.d/99-fasschema.ldif /usr/share/ipa/schema.d/
+cp schema.d/89-fasschema.ldif /usr/share/ipa/schema.d/
+rm -f /usr/share/ipa/schema.d/99-fasschema.ldif
 
-cp updates/99-fas.update /usr/share/ipa/updates/
+cp updates/89-fas.update /usr/share/ipa/updates/
+rm -f /usr/share/ipa/updates/99-fas.update
 
 mkdir -p -m 755 /usr/share/ipa/ui/js/plugins/userfas
 cp ui/js/plugins/userfas/userfas.js /usr/share/ipa/ui/js/plugins/userfas/
@@ -28,8 +30,8 @@ if [ $NEEDS_UPGRADE = 1 ]; then
     ipa-server-upgrade
 else
     ipa-ldap-updater \
-        -S /usr/share/ipa/schema.d/99-fasschema.ldif \
-        /usr/share/ipa/updates/99-fas.update
+        -S /usr/share/ipa/schema.d/89-fasschema.ldif \
+        /usr/share/ipa/updates/89-fas.update
     systemctl restart httpd.service
 fi
 
